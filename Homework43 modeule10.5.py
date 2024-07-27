2665effb34ee9196a19ae8486a397477d0248578
MaaaaD1122 <164404426+MaaaaD1122@users.noreply.github.com>
2024-07-27 22:07:35 +0300

    Add files via upload

---
+++ Homework43 modeule10.5.py
@@ -0,0 +1,49 @@
+import multiprocessing
+
+
+class WarehouseManager:
+    def __init__(self):
+        self.data = multiprocessing.Manager().dict()
+
+    def process_request(self, request):
+        product_name, action, quantity = request
+
+        if action == "receipt":
+            current_quantity = self.data.get(product_name, 0)
+            self.data[product_name] = current_quantity + quantity
+
+        elif action == "shipment":
+            current_quantity = self.data.get(product_name, 0)
+            if current_quantity > 0:
+                new_quantity = max(current_quantity - quantity, 0)
+                self.data[product_name] = new_quantity
+
+    def run(self, requests):
+        processes = []
+        for request in requests:
+            i = multiprocessing.Process(target=self.process_request, args=(request,))
+            processes.append(i)
+            i.start()
+
+        for i in processes:
+            i.join()
+
+
+if __name__ == "__main__":
+    # Создаем менеджера склада
+    manager = WarehouseManager()
+
+    # Множество запросов на изменение данных о складских запасах
+    requests = [
+        ("product1", "receipt", 100),
+        ("product2", "receipt", 150),
+        ("product1", "shipment", 30),
+        ("product3", "receipt", 200),
+        ("product2", "shipment", 50)
+    ]
+
+    # Запускаем обработку запросов
+    manager.run(requests)
+
+    # Выводим обновленные данные о складских запасах
+    print(dict(manager.data))
